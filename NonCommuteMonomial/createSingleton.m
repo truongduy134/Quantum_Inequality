3e31909eb72f41384b75e28c874c3c771b406071
Duy NguyenTruong <truongduy134@yahoo.com>
2013-02-01 06:30:08 +0800

    Upload new one (Step 1: Delete all)

--- NonCommuteMonomial/createSingleton.m
+++
@@ -1,8 +0,0 @@
-% Function: createSingleton(varNo, paritionNo, numPartition)
-% 
-% Create a singleton monomial structure.
-function singletonMono = createSingleton(varNo, partitionNo, numPartition)
-	varOrdering = cell(1, numPartition);
-	varOrdering{partitionNo} = varNo;
-	singletonMono = NonCommuteMonomial(varOrdering, 1);
-end
